0fe2388617660a6af9bde0345adf2b0d4f74be81
ikaslea <ikaslea@DESKTOP-7UVSHRO>
2020-11-17 10:37:18 +0100

    menú 4 opciones implementado

--- menuakGit/src/menuakGit/menuak.java
+++ menuakGit/src/menuakGit/menuak.java
@@ -1,10 +1,48 @@
 package menuakGit;
-
+import java.util.Scanner;
 public class menuak {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int elec,x,y;
+		
+		System.out.println("menu \r\n"
+				+ "1-suma \r\n"
+				+ "2-resta \r\n"
+				+ "3-multiplicar \r\n"
+				+ "4-dividir");
+		elec = input.nextInt();
+		switch(elec) {
+		case 1: 
+			System.out.println("Primer numero");
+			x = input.nextInt();
+			System.out.println("Segundo numero");
+			y = input.nextInt();
+			System.out.println(x+y);
+			break;
+		case 2:
+			System.out.println("Primer numero");
+			x = input.nextInt();
+			System.out.println("Segundo numero");
+			y = input.nextInt();
+			System.out.println(x-y);
+			break;
+		case 3:
+			System.out.println("Primer numero");
+			x = input.nextInt();
+			System.out.println("Segundo numero");
+			y = input.nextInt();
+			System.out.println(x*y);
+			break;
+		case 4:
+			System.out.println("Primer numero");
+			x = input.nextInt();
+			System.out.println("Segundo numero");
+			y = input.nextInt();
+			System.out.println(x/y);
+			break;
+		}
 	}
 
 }
